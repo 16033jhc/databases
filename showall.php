74c651e319e37d71196ff815ae1b8e6ebb626c9f
16033 <16033@jhc.school.nz>
2021-06-23 10:41:52 +1200

    added php to display results in showall.php

--- showall.php
+++ showall.php
@@ -1,11 +1,50 @@
-<?php include("topbit.php") ?>
+<?php include("topbit.php");
+
+    $find_sql = "SELECT * FROM `gamedata`";
+    $find_query = mysqli_query($dbconnect, $find_sql);
+    $find_rs = mysqli_fetch_assoc($find_query);
+    $count = mysqli_num_rows($find_query);
+?>
 			
 			<div class="box main">
 				<h2>All Results</h2>
 				
-				<p>
-					Results go here.
-				</p>
+				<?php
+
+                if($count < 1) {
+
+                    ?>
+
+                <div class="error">
+                   
+                    There are no results that match your query.
+                    Please use the search box to try again.
+                
+                </div> <!-- end error -->
+
+                <?php
+                } // end no results if
+
+                else {
+                    do
+                    {
+
+                        ?>
+                <!-- Results go here -->
+                <div class="results">
+                    You have mail!
+                </div>
+
+                <?php
+                    
+                    } // end results 'do'
+
+                    while
+                        ($find_rs=mysqli_fetch_assoc($find_query));
+
+                } // end else
+
+                ?>
 			
             </div> <!-- / main -->
 
