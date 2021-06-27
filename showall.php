7fd5dc1a121a448cdb7bca252f174a141903043e
16033 <16033@jhc.school.nz>
2021-06-28 10:59:54 +1200

    fixed broken developer join

--- showall.php
+++ showall.php
@@ -1,13 +1,34 @@
 <?php include("topbit.php");
 
     $find_sql = "SELECT * FROM `gamedata`
-    JOIN genres ON (gamedata.GenreID = genres.GenreID)
+    JOIN genres ON (gamedata.GenreID = genres.GenreID) 
+    JOIN devs ON (gamedata.DevID = devs.DevID)
+    LIMIT 50
     ";
     $find_query = mysqli_query($dbconnect, $find_sql);
     $find_rs = mysqli_fetch_assoc($find_query);
     $count = mysqli_num_rows($find_query);
 ?>
-			
+
+<?php function format_percentage($percentage, $precision = 2) {
+    return round($percentage, $precision) . '%';
+}
+
+function calculate_percentage($number, $total) {
+
+    // Can't divide by zero so let's catch that early.
+    if ($total == 0) {
+        return 0;
+    }
+
+    return ($number / $total) * 100;
+}
+
+function percent($number, $total) {
+    return format_percentage(calculate_percentage($number, $total));
+}
+?>
+
 			<div class="box main">
 				<h2>All Results</h2>
 				
@@ -39,11 +60,21 @@
                             <?php echo $find_rs['name']; ?>
                         </a>
                     </span>
+                    
+                    <p>
+                        <b>Genre</b>:
+                        <?php echo $find_rs['genres']; ?>
 
-                    <br />
-                    <?php echo $find_rs['GenreID']; ?>
-                    <br />
-                    <?php echo $find_rs['genres']; ?>
+                        <br />
+
+                        <b>Developer</b>:
+                        <?php  echo $find_rs['developer']; ?>
+
+                        <br />
+
+                        <b>Rating</b>:
+                        <?php percent($value['positive_reviews'], (['positive_reviews'] + ['negative_reviews'])) ?>
+                    </p>
 
                 </div> <!-- results -->
 
