9937eeb06edca15c835cf99768cc7cdd00c4a845
16033 <16033@jhc.school.nz>
2021-11-16 13:22:31 +1300

    added insertData.php, added form to bottombit.php

--- bottombit.php
+++ bottombit.php
@@ -6,7 +6,7 @@
             <form class="searchform" method="post"
             action="search.php" enctype="multipart/form-data">
 
-                <input class="search" type="text" name="dev_name"
+                <input class="search" type="text" name="typeID"
                 size="40" value="" required placeholder="App Name / Dev Name"/>
 
                 <input class="submit" type="submit"
@@ -17,16 +17,60 @@
             
             <div class="advanced-frame">
                 
-                <h2>Advanced Search</h2>
+                <h2>Add New Data</h2>
                 
-                <form class="searchform" method="post"
-                action="advanced.php" enctype="multipart/form-data">
-                    
-                    <p>Form goes here</p>
+                <form class="add_data" action="insertData.php" method="post" name="insert">
+                    <fieldset id="fields">
+                        <legend>Insert New Entry</legend>
 
-                    <input class="submit advanced-button" type="submit"
-                    name="advanced" value="Search &nbsp; &#xf002;" />
+                            <label for="nameText">Name:</label></br>
+                            <input name="nameText" id="nameText" type="text"></input>
+                            
+                            </br>
 
+                            <label for="typeText">Type(s):</label></br>
+                            <input name="typeText" id="typeText" type="text"></input>
+                            
+                            </br>
+
+                            <label for="healthNumber">Health Points:</label>
+                            <input name="healthNumber" id="healthNumber" type="number"></input>
+                            
+                            </br>
+                            
+                            <label for="attackNumber">Attack Points:</label>
+                            <input name="attackNumber" id="attackNumber" type="number"></input>
+                            
+                            </br>
+                            
+                            <label for="defenseNumber">Defense Points:</label>
+                            <input name="defenseNumber" id="defenseNumber" type="number"></input>
+                            
+                            </br>
+                           
+                            <label for="Sp_attackNumber">Special Attack Points:</label>
+                            <input name="Sp_attackNumber" id="Sp_attackNumber" type="number"></input>
+                            
+                            </br>
+                           
+                            <label for="Sp_defenseNumber">Special Defense Points:</label>
+                            <input name="Sp_defenseNumber" id="Sp_defenseNumber" type="number"></input>
+                            
+                            </br>
+                            
+                            <label for="speedNumber">Speed Points:</label>
+                            <input name="speedNumber" id="speedNumber" type="number"></input>
+                            
+                            </br>
+                            
+                            <label for="generationNumber">Generation:</label>
+                            <input name="generationNumber" id="generationNumber" type="number"></input>
+                    </fieldset>
+
+                    <fieldset>
+                        <input type="submit" value="Submit Data" class="button">
+                        <input type="reset" value="Reset" class="button">
+                    </fieldset>
                 </form>
 
                 </div> <!-- / advanced frame -->
---
+++ insertData.php
@@ -0,0 +1,64 @@
+<form class="add_data" action="insertData.php" method="post" name="insert">
+    <fieldset id="fields">
+        <legend>Insert New Entry</legend>
+        
+        <label for="nameText">Name:</label>
+        <input name="nameText" id="nameText" type="text"></input>
+        
+        <label for="typeText">Type(s):</label>
+        <input name="typeText" id="typeText" type="text"></input>
+        
+        <label for="healthNumber">Health Points:</label>
+        <input name="healthNumber" id="healthNumber" type="number"></input>
+        
+        <label for="attackNumber">Attack Points:</label>
+        <input name="attackNumber" id="attackNumber" type="number"></input>
+        
+        <label for="defenseNumber">Defense Points:</label>
+        <input name="defenseNumber" id="defenseNumber" type="number"></input>
+       
+        <label for="Sp_attackNumber">Special Attack Points:</label>
+        <input name="Sp_attackNumber" id="Sp_attackNumber" type="number"></input>
+       
+        <label for="Sp_defenseNumber">Special Defense Points:</label>
+        <input name="Sp_defenseNumber" id="Sp_defenseNumber" type="number"></input>
+      
+        <label for="speedNumber">Speed Points:</label>
+        <input name="speedNumber" id="speedNumber" type="number"></input>
+      
+        <label for="generationNumber">Generation:</label>
+        <input name="generationNumber" id="generationNumber" type="number"></input>
+    </fieldset>
+
+    <fieldset>
+        <input type="submit" value="Submit Data" class="button">
+        <input type="reset" value="Reset" class="button">
+    </fieldset>
+</form>
+
+
+<?php
+
+    require_once 'config.php';
+
+    $name = $_REQUEST['nameText']
+    $types = $_REQUEST['typeText']
+    $health = $_REQUEST['healthNumber']
+    $attack = $_REQUEST['attackNumber']
+    $defense = $_REQUEST['defenseNumber']
+    $sp_attack = $_REQUEST['Sp_attackNumber']
+    $sp_defense = $_REQUEST['Sp_defenseNumber']
+    $speed = $_REQUEST['speedNumber']
+    $generation = $_REQUEST['generationNumber']
+
+    $sql = "INSERT INTO details (Name, HP, Attack, Defense, Sp. Atk, Sp. Def, Speed, Generation)
+            VALUES ('$name', '$health', '$defense', '$sp_attack', '$sp_defense', '$speed', '$generation')"; 
+
+    
+    // Checks record was added
+    
+    if($dbconnect->query($sql) == true){
+        echo "New entry added";
+    } else{ 
+        echo "Error: " . $sql . "<br>" . $dbconnect->error;
+    }
